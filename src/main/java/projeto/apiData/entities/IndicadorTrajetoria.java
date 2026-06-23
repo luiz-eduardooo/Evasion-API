759cb22e46282c210cc26f2ac358f7f2fc614e5f
luiz-eduardooo <luizeduardo2013051@gmail.com>
2026-06-22 23:13:22 -0300

    feat: adição do repositorio e entidade

---
+++ src/main/java/projeto/apiData/entities/IndicadorTrajetoria.java
@@ -0,0 +1,46 @@
+package projeto.apiData.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "indicador_trajetoria")
+@Getter
+@Setter
+public class IndicadorTrajetoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private Integer coCurso;
+    private String  noCurso;
+    private Long    coIes;
+    private String  noIes;
+
+    private Integer coRegiao;
+    private Integer coUf;
+    private Integer tpCategoriaAdministrativa;
+    private Integer tpModalidadeEnsino;
+    private Integer coCineAreaGeral;
+    private String  noCineAreaGeral;
+
+    private Integer nuAnoIngresso;
+    private Integer nuAnoReferencia;
+
+    private Integer qtIngressante;
+    private Integer qtPermanencia;
+    private Integer qtConcluinte;
+    private Integer qtDesistencia;
+    private Integer qtFalecido;
+
+
+    private Double tda;
+    private Double tca;
+}
---
+++ src/main/java/projeto/apiData/repositories/IndicadorTrajetoriaRepository.java
@@ -0,0 +1,7 @@
+package projeto.apiData.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projeto.apiData.entities.IndicadorTrajetoria;
+
+public interface IndicadorTrajetoriaRepository extends JpaRepository<IndicadorTrajetoria, Long> {
+}
